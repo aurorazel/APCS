6cb81db62fa751d10844af289a81e9b83b8e960c
Aurora Zeltser <aurorazeltser@Auroras-MacBook-Air.local>
2020-03-31 18:49:51 -0700

    edit

--- FR2019/StepTracker.java
+++ FR2019/StepTracker.java
@@ -2,11 +2,14 @@ import java.util.*;
 
 public class StepTracker{
     private int minSteps, totalSteps, activeDays, totalDays;
-    public StepTracker(int mn) {minSteps=mn;} 
+    public StepTracker(int mn) {minSteps=mn;}
     public void addDailySteps(int s){
         totalSteps+=s; totalDays++;
         if (s>=minSteps) activeDays++;
     }
     public int activeDays() {return activeDays;}
-    public double averageSteps() {return (double)totalSteps/totalDays;}
+    public double averageSteps(){
+        if (totalDays==0) return 0; 
+        return (double)totalSteps/totalDays;
+    }
 }
