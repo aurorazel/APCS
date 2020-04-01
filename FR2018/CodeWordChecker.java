29ce8563597cbebf29abace3a66857f736968dbe
Aurora Zeltser <aurorazeltser@Auroras-MacBook-Air.local>
2020-03-31 18:41:28 -0700

    updated Q3

--- FR2018/CodeWordChecker.java
+++ FR2018/CodeWordChecker.java
@@ -6,6 +6,7 @@ public interface StringChecker{ // public ?
 public class CodeWordChecker implements StringChecker{
     private int minLen,maxLen;
     private String badStr;
+    public CodeWordChecker(String bs) {minLen=6; maxLen=12; badStr=bs;} 
     public CodeWordChecker(int mn,int mx,String bs) {minLen=mn; maxLen=mx; badStr=bs;}
     public boolean isValid(String str){
         int l=str.length();
