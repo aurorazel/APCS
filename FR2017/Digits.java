57e67a00676f4bc3ad36680162fd4e2ceaaef530
Aurora Zeltser <aurorazeltser@Auroras-MacBook-Air.local>
2020-04-01 12:41:54 -0700

    edit Q1

--- FR2017/Digits.java
+++ FR2017/Digits.java
@@ -3,6 +3,7 @@ public class Digits{
     private ArrayList<Integer> digitList;
     public Digits(int n){
         digitList=new ArrayList<Integer>();
+        if (n==0) {digitList.add(0);}
         while (n>0){
             digitList.add(0,n%10);
             n/=10;
@@ -10,7 +11,7 @@ public class Digits{
     }
     public boolean isStrictlyIncreasing(){
         for (int i=1; i<digitList.size(); i++){
-            if (digitList.get(i-1)>=digitList.get(i)) return false;
+            if (digitList.get(i-1).intValue()>=digitList.get(i).intValue()) return false;
         }
         return true;
     }
